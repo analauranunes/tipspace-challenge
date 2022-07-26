0a2348a238f9a8145b80e04944209c5e52c4ee19
Ana Laura Nunes <analaurahdn@gmail.com>
2022-07-26 16:30:14 -0300

    feat: comments provider created along with functions to retrieve and add new comments.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/providers/comments/index.js
@@ -0,0 +1,72 @@
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  setDoc,
+  where,
+} from "firebase/firestore";
+import { createContext, useContext, useState } from "react";
+import { DataBaseContext } from "../database";
+import { toast } from "react-toastify/";
+
+export const CommentsContext = createContext();
+
+function CommentsProvider({ children }) {
+  const { db } = useContext(DataBaseContext);
+
+  const [pokeComments, setPokeComments] = useState();
+
+  async function newComment(pokeName, name, email, comment) {
+    const pokeCollection = doc(db, "comments", email);
+    const pokeDoc = await getDoc(pokeCollection);
+
+    if (pokeDoc.exists()) {
+      toast.error("E-mail já utilizado para comentar anteriormente!");
+    }
+
+    await setDoc(doc(collection(db, "comments"), email), {
+      name: name,
+      email: email,
+      comment: comment,
+      pokename: pokeName,
+    });
+
+    toast.success("Comentário adicionado com sucesso!");
+  }
+
+  async function allPokeComments(pokeName) {
+    const pokeCollection = collection(db, "comments");
+    const selectAllComments = query(
+      pokeCollection,
+      where("pokename", "==", pokeName)
+    );
+    const allComments = await getDocs(selectAllComments);
+
+    if (allComments) {
+      allComments.forEach((comment) => {
+        setPokeComments([...pokeComments, comment]);
+      });
+    }
+  }
+
+  function clearPokeComments() {
+    setPokeComments(null);
+  }
+
+  return (
+    <CommentsContext.Provider
+      value={{
+        newComment,
+        allPokeComments,
+        clearPokeComments,
+        pokeComments,
+      }}
+    >
+      {children}
+    </CommentsContext.Provider>
+  );
+}
+
+export default CommentsProvider;
