521b98cb0ff4985d0af7132a64134e4c6e568d42
Ana Laura <analaurahdn@gmail.com>
2022-07-27 14:59:09 -0300

    style: Background-color set to root.

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -7,6 +7,11 @@ export const GlobalStyle = createGlobalStyle`
         border: none;
         font-family: 'Chakra Petch', sans-serif;
     }
+    
+    :root{
+        background-color: orange;
+    }
+
     h1{
         font-family: 'Amatic SC', cursive;
     }
